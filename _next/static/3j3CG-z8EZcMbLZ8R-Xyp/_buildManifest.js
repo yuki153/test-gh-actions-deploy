f0232864f06595e0f543ab045ecabdd1ffc1e6c7
yuki153 <yuki153@users.noreply.github.com>
2022-05-21 19:01:46 +0000

    deploy: f63de77f1109795a4dda2207ee45a881fe3f3a41

---
+++ _next/static/3j3CG-z8EZcMbLZ8R-Xyp/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/25fe44bcb832225f.css","static/chunks/pages/index-46c36c687bd44df5.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
