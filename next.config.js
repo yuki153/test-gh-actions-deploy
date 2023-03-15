1460e6d90e7097b4dd1b4dcb71d41c938502195c
yuki153 <37236474+yuki153@users.noreply.github.com>
2023-03-16 00:38:07 +0900

    chore: 補足コメント追記

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // Github Pages に host された時の url がサブディレクトリ構造となるため 
   basePath: process.env.NODE_ENV === "production" ? "/test-gh-actions-deploy" : "",
   reactStrictMode: true,
 }
